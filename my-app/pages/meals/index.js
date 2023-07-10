5596f197be92d52b78d81f80c84e8aec2467cfcc
espnal <78657757+espnal@users.noreply.github.com>
2023-07-09 23:27:51 -0400

    new update

--- my-app/pages/meals/index.js
+++ my-app/pages/meals/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import SearchBar from '../../componets/mealspage/SearchBar';
 import classes from './meals.module.scss';
+import PointText from '../../componets/text/PointText';
 
-function index() {
+
+function Meals() {
+  const result = useQuery(['categories'], getCategories)
   return (
-    <div>index</div>
+    <div className={classes.meals__page}>
+      <SearchBar/>
+      <PointText className={classes.text} >Search meals or select categories from below.</PointText>
+    </div>
   )
 }
 
-export default index
+export default Meals
