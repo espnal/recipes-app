921aa6426099c0a55099ea33bc39bfdc6806698c
espnal <78657757+espnal@users.noreply.github.com>
2023-07-22 11:36:28 -0400

    latest changes

--- my-app/pages/meals/index.js
+++ my-app/pages/meals/index.js
@@ -20,25 +20,55 @@ const getMeals = async ({queryKey}) => {
   const {data} = await axios.get(`/filter.php?c=${queryKey[1]}`)
   return data?.meals || [];
 }
+const getQueriedMeals = async({queryKey})=>{
+  const {data} = await axios.get(`search.php?s=${queryKey[1]}`)
+console.log(data?.meals || []);
+}
 function Meals() {
   const [selectedCategory,setSelectedCategory] = useState("");
+  const [searchText, setSearchText] = useState('');
+  const [query, setQuery] = useState('');
 
-  const {data: categories, 
-    isLoading: categoriesIsLoading, 
-    isError: categoriesIsError} = useQuery(['categories'], getCategories)
+  const {data: categories, isLoading: categoriesIsLoading, isError: categoriesIsError} = useQuery(['categories'], getCategories)
   
-  const {data, isLoading, isError} = useQuery(['mealsByCategory', selectedCategory], getMeals);
+  const {data: queriedData, isLoading: queryIsLoading} = useQuery(['mealsByQuery', query], getQueriedMeals, {
+    enabled:query !=="",
+  })
+
+  const {data, isLoading, isError} = useQuery(['mealsByCategory', selectedCategory], getMeals, {
+    enabled:query ==="",
+  });
+  
+
+
+  useEffect(()=>{
+    const timeout = setTimeout(()=>{
+      if(searchText){
+        setQuery(searchText);
+        setSelectedCategory('')
+      }else{
+        setQuery('')
+        if(categories){
+          setSelectedCategory(categories[0].strCategory)
+        }
+      }
+    },300);
+    return()=>{
+      setQuery('');
+      clearTimeout(timeout);
+    }
+  },[searchText, categories])
+
   useEffect(()=>{
     if(categories){
       setSelectedCategory(categories[0].strCategory)
     }
   },[categories] )
-  const [searchText, setSearchText] = useState('');
-  const [query, setQuery] = useState('');
+
 
     return (
     <div className={classes.meals__page}>
-      <SearchBar/>
+      <SearchBar seachText={searchText} setSearchText={setSearchText}/>
       <PointText className={classes.text} >Search meals or select categories from below.</PointText>
     <Categories 
     categories={categories} 
@@ -52,10 +82,15 @@ function Meals() {
       <div className={classes.loadingSpinner}>
       <BeatLoader color="#fff" loading={isLoading || categoriesIsLoading} cssOverride={override} size={20}/>
       </div>) : null}
+
       <div className={classes.meals__container}>
       {!isLoading && !isError && data && data.map((meal)=>(
-        <SingleMealCard meal={meal} key={meal.idMeal}/>
+        <SingleMealCard key={meal.idMeal} meal={meal}/>
       ))}
+
+      {!queryIsLoading && queriedData && queriedData.map((meal)=>{
+        <SingleMealCard key={meal.idMeal} meal={meal}/>
+      })}
       </div>
     </div>
   )
